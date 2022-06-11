14dd2c5953097ef2fce0e0711e94cca7c722d59a
NguyenCaoDuyLinh <94783468+NguyenCaoDuyLinh@users.noreply.github.com>
2022-06-11 09:14:31 +0700

    Add files via upload

---
+++ B1/B1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap so nguyen bat ky: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 0)
+                Console.WriteLine("day la so nguyen duong");
+            else
+                Console.WriteLine("day la so nguyen âm");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ B1/b4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace b4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap a: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("nhap b: ");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write("nhap c: ");
+            float c = float.Parse(Console.ReadLine());
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+         
+                if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == b * b + a * a))
+                    Console.WriteLine("day la tam giac vuong");
+                else
+                    Console.WriteLine("day khong phai la tam giac vuong");
+            }
+            else
+                Console.WriteLine("{0}, {1}, {2} khong phai là 3 canh cua 1 tam giac", a, b, c);
+
+            Console.ReadKey();
+        }
+    }
+}
